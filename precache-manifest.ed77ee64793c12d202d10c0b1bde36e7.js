99df3649a05960db87ed64e27971816418374e81
Eldhose Kuriakose <56919546+EldhoseKuriakose@users.noreply.github.com>
2020-06-28 13:56:34 +0530

    Updates

---
+++ precache-manifest.ed77ee64793c12d202d10c0b1bde36e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bfd2e6baff0f6c0707639f16bcf612e",
+    "url": "/Monsters-Rolodex/index.html"
+  },
+  {
+    "revision": "05108613cb87dec8450a",
+    "url": "/Monsters-Rolodex/static/css/main.96102a41.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/Monsters-Rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Monsters-Rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05108613cb87dec8450a",
+    "url": "/Monsters-Rolodex/static/js/main.78745078.chunk.js"
+  },
+  {
+    "revision": "bb1db29ffdd7a4ded20c",
+    "url": "/Monsters-Rolodex/static/js/runtime-main.f852ebba.js"
+  }
+]);
